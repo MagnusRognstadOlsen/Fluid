5ec84c62f51941d46abd1a280218b64eb8a5ab8c
MagnusRognstadOlsen <magnus.rognstad.olsen@nhn.no>
2025-02-11 13:37:23 +0100

    Add files via upload

---
+++ C# - Plattform/Fluid_Test/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.RegularExpressions;
+using Fluid;
+
+var parser = new FluidParser();
+
+var model = new { Firstname = "Bill", Lastname = "Gates" };
+var source = "Hello {{ Firstname }} {{ Lastname }}";
+
+if (parser.TryParse(source, out var template, out var error))
+{
+    var context = new TemplateContext(model);
+
+    Console.WriteLine(template.Render(context));
+}
+else
+{
+    Console.WriteLine($"Error: {error}");
+}
